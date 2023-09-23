3073d881720246c59316bb17ab7267aa6d6753b7
CastleBasset <145203142+CastleBasset@users.noreply.github.com>
2023-09-23 16:40:44 +0300

    a*b +c

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -2,21 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
 
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-        String WelcomStr;//Declaration
+        String WelcomStr;
         WelcomStr="Hello world";
-        
         System.out.println(WelcomStr);
 
+        String Name = "Alexandr";
+        String Surname = "Kapralov";
+        String space = " ";
+        System.out.println(Name + space + Surname);
+
+        int a = 2;
+        int b = 3;
+        int r = 5;
+        int result = a * r + b;
+        System.out.println("a * r + b = " + result);
     }
 }
