8179bc781b40cab5b0321c5c62a90e725d8525b5
CastleBasset <145203142+CastleBasset@users.noreply.github.com>
2023-10-21 18:29:00 +0300

    divide and summarize

--- lesson1/src/Main.java
+++ lesson1/src/Main.java
@@ -2,45 +2,22 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-
-
-//        String WelcomStr;
-//        WelcomStr="Hello world";
-//        System.out.println(WelcomStr);
-//
-//        String Name = "Alexandr";
-//        String Surname = "Kapralov";
-//        String space = " ";
-//        System.out.println(Name + space + Surname);
-//
-//        int a = 2;
-//        int b = 3;
-//        int r = 5;
-//        int result = a * r + b;
-//        System.out.println("a * r + b = " + result);
-
         Scanner input = new Scanner(System.in);
-//       int a = input.nextInt();
-//       int b = input.nextInt();
-//       int c = input.nextInt();
-//       int res =  a * c + b;
-//       System.out.println("я ввёл: " + res);
-//
-//        String name = input.nextLine();
-  //      String surname = input.nextLine();
-    //    String fullName = name + " " + surname;
-        //  System.out.println("this program did:" + fullName);
-        int a = input.nextInt();
-  //      int b = input.nextInt() ;
-        if ( a % 2 == 0 )
-        {
-            System.out.println( "Yes" );
+        System.out.print("Введите начальное число: ");
+        int start = input.nextInt();
+        System.out.print("Введите конечное число: ");
+        int end = input.nextInt();
+        System.out.print("Введите делитель: ");
+        int divider = input.nextInt();
+        int res = 0;
+        for (; start <= end; start++) {
+            if (start % divider == 0) {
+                res += start;
+            }
         }
-        else
-        {
-            System.out.println( "No" );
-        }
+        System.out.println("Сумма чисел от " + start + " до " + end + " кратных " + divider + " равна " + res);
     }
 }
